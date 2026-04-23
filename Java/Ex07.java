4298a3135644628b532d538197fab0b4297e14f9
fnbueno <fnbuenoofficial@gmail.com>
2026-04-23 14:21:14 -0300

    Add files via upload

---
+++ Java/Ex07.java
@@ -0,0 +1,10 @@
+package javaapplication1;
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        Casa c = new Casa(1, "rua X", 500, 200);
+        c.getInfo();
+        Imovel i = new Casa(2, "y", 0, 100);
+        i.getInfo();
+    }
+    
+}
